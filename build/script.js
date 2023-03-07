71b37fbe3287c97b94e3ce47503d3635064f9a64
Marcote <marcotedev@gmail.com>
2023-03-07 15:57:26 +0100

    Update

--- build/script.js
+++ build/script.js
@@ -2,10 +2,73 @@
 import { randomWord as _randomWord } from "../src/words.js";
 let randomWord = _randomWord;
 
-document.querySelectorAll(".letter").forEach((element) => {
-    element.addEventListener("click", () => {
-        element.innerHTML = "A"
-    })
-})
+const state = {
+    grid: Array(6)
+        .fill()
+        .map(() => Array(5).fill("")),
+    currentRow: 0,
+    currentCol: 0
+};
 
-$("#word").text(randomWord());
+function update() {
+    for (let i = 0; i < state.grid.length; i++) {
+        for (let j = 0; j < state.grid[i].length; j++) {
+            const box = document.getElementById(`box${i}${j}`);
+            box.textContent = state.grid[i][j];
+        }
+    }
+}
+
+function draw(container, row, col, letter = "") {
+    const box = document.createElement('div');
+    box.className = 'box';
+    box.textContent = letter;
+    box.id = `box${row}${col}`;
+    container.appendChild(box);
+    return box;
+}
+
+function drawGame(container) {
+    const grid = document.createElement('div');
+    grid.className = 'grid';
+    for (let i = 0; i < 6; i++) {
+        for (let j = 0; j < 5; j++) {
+            draw(grid, i, j);
+        }
+    }
+
+    container.appendChild(grid);
+}
+
+function registerKey() {
+    document.body.onkeydown = (e) => {
+        const key = e.key;
+        if(key === "Enter"){
+        }
+        if(key === "Backspace"){
+
+        }
+        if(isLetter(key)){
+
+        }
+        updateGrid();
+    };
+}
+
+function main() {
+    const game = document.getElementById("game");
+    drawGame(game);
+    registerKey();
+}
+
+main();
+
+
+
+// document.querySelectorAll(".letter").forEach((element) => {
+//     element.addEventListener("click", async () => {
+//         element.innerHTML = "A";
+//     })
+// })
+
+// $("#word").text(randomWord());
